2bba837a8952747aefedd6d772ac19214f4544ed
jonathanhand <hand.jonathan.13@gmail.com>
2019-05-16 15:39:06 -0700

    switched to auto updating list

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
-    var userEmail;
+    var userEmail = 'Jonathan Hand';
+    var contact;
 
     function googleLogin() {
     const provider = new firebase.auth.GoogleAuthProvider()
@@ -27,4 +28,29 @@
       username: name,
     });
   }
-  //unique code>name, email, phone
+
+  function getUsers() {
+      getUserData(userEmail);
+  }
+
+    // var userId = firebase.database().ref('/' + login + '/')
+    // .on('value', function(snapshot)) {
+    //   printList(snapshot.val())
+    // });
+
+    var starCountRef = firebase.database().ref('/' + userEmail + '/');
+    starCountRef.on('value', function(snapshot) {
+      printList(snapshot.val());
+    });
+    
+  function printList(contact) {
+    // for (var id in contact) {
+        for (each in contact) {
+        console.log(contact[each].username);
+        }
+    //   makeTable(snapshotDB[id], target);
+      // addDetailsBtn(id, target);
+      // addEditBtn(id, target);
+      // addDeleteBtn(id, target);
+    // }
+  }
